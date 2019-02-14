12032b4bb5bc77e2b133db4a4fd7b994196c453f
Ananya Goel <ananya.goel10@gmail.com>
2019-02-14 09:15:48 +0530

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -1,36 +1,38 @@
 package com.code.util;
 
-
 import com.code.io.Input;
 
 public class Operations {
-  
-  public int operation(int[][]arrr, int row, int col)
-  {
-/*    Input myObj = new Input(inputObj);
-    int [][] arr = myObj.getMatrix().clone();
-   System.out.println(myObj.hashCode() + "  " + myObj.getMatrix());
-    System.out.println(inputObj.hashCode() + " " + inputObj.getMatrix());
-    System.out.println(arr);
-*/    int max = -1;
-    
-int[][] arr = arrr.clone();
-    for(int i=row; i<4; i++)
-    {
-      for(int j=col; j<4; j++)
-      {
-        if(i>row&&j>col)
-          arr[i][j]=arr[i][j-1]+arr[i-1][j]+arr[i][j]-arr[i-1][j-1];
-        else if(i>row&&j==col)
-          arr[i][j]=arr[i][j]+arr[i-1][j];
-        else if(i==row && j>col)
-          arr[i][j]=arr[i][j]+arr[i][j-1];
-        else if(i==row&&j==col)
-          arr[i][j]=arr[i][j];
-        
-        if(arr[i][j]>max) max = arr[i][j];
-      }
-    }
-    return max;
-  }
+
+	public int operation(Input inputObj, int row, int col) {
+		int[][] arr = copyArray(inputObj.getMatrix());
+		int max = -1;
+		for (int i = row; i < inputObj.getNoOfRows(); i++) {
+			for (int j = col; j < inputObj.getNoOfColumns(); j++) {
+				if (i > row && j > col)
+					arr[i][j] = arr[i][j - 1] + arr[i - 1][j] + arr[i][j] - arr[i - 1][j - 1];
+				else if (i > row && j == col)
+					arr[i][j] = arr[i][j] + arr[i - 1][j];
+				else if (i == row && j > col)
+					arr[i][j] = arr[i][j] + arr[i][j - 1];
+				else if (i == row && j == col)
+					arr[i][j] = arr[i][j];
+
+				if (arr[i][j] > max)
+					max = arr[i][j];
+			}
+		}
+		return max;
+	}
+
+	public static int[][] copyArray(int[][] arr) {
+		int[][] result = new int[arr.length][arr[0].length];
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				result[i][j] = arr[i][j];
+			}
+		}
+		return result;
+	}
 }
