ee83fda439532d2ce4689e0e94db37f811871d07
Ananya Goel <ananya.goel10@gmail.com>
2019-02-14 09:14:52 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,24 +6,20 @@ import com.code.util.Operations;
 
 public class Main {
 
-  public static void main(String[] args)
-  {
-    Operations operationsObj = new Operations();
-    
-   // Input inputObj = new Input();
-    
-    int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-    
-    for(int i=0; i<4; i++)
-    {
-      for(int j=0; j<4; j++)
-      {
-/*          Output.printMat(inputObj.getMatrix());
-          Output.printInt(operationsObj.operation(inputObj, 2, 2));
-*/                Output.printMat(matrix);
-Output.printInt(operationsObj.operation(matrix, 2, 2));
+	public static void main(String[] args) {
+		Operations operationsObj = new Operations();
 
-        }
-    }
-  }
+		Input inputObj = new Input();
+
+		int max = -1;
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if ((operationsObj.operation(inputObj, i, j)) > max)
+					max = operationsObj.operation(inputObj, i, j);
+			}
+		}
+
+		Output.printInt(max);
+	}
 }
