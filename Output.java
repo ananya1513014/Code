d9561d6f0031a6a659a25be294d7c571c11da288
Ananya Goel <ananya.goel10@gmail.com>
2019-02-14 09:17:05 +0530

    Update Output.java

--- Output.java
+++ Output.java
@@ -2,20 +2,16 @@ package com.code.io;
 
 public class Output {
 
-  public static void printMat(int[][] arr)
-  {
-    for(int i=0; i<arr.length; i++)
-    {
-      for(int j=0; j<arr[0].length; j++)
-      {
-        System.out.print(arr[i][j] + " ");
-      }
-      System.out.println();
-    }
-  }
-  
-  public static void printInt(int num)
-  {
-    System.out.println(num);
-  }
+	public static void printMat(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void printInt(int num) {
+		System.out.println(num);
+	}
 }
